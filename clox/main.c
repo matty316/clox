8a0bd1c9008c6194d7534c7b9e38394a44090f8f
matty <4nem@matty316.com>
2023-10-30 00:55:23 -0500

    Initial Commit

---
+++ clox/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  clox
+//
+//  Created by matty on 10/30/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
