ea435528b8cec69152bd158cc8643fbfd6291923
matty <4nem@matty316.com>
2023-10-30 20:54:51 -0500

    add consts

--- clox/chunk.h
+++ clox/chunk.h
@@ -9,19 +9,36 @@
 #define chunk_h
 
 #include "common.h"
+#include "value.h"
 
 typedef enum {
+    OP_CONSTANT,
+    OP_CONSTANT_LONG,
     OP_RETURN,
 } OpCode;
+
+
+typedef struct {
+    int offset;
+    int line;
+} LineStart;
 
 typedef struct {
     int count;
     int capacity;
+    ValueArray constants;
     uint8_t* code;
+    int lineCount;
+    int lineCapacity;
+    LineStart *lines;
 } Chunk;
+
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+int addConstant(Chunk* chunk, Value value);
+int getLine(Chunk* chunk, int instruction);
+void writeConstant(Chunk* chunk, Value value, int line);
 
 #endif /* chunk_h */
--- clox/debug.c
+++ clox/debug.c
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 #include "debug.h"
+#include "value.h"
 
 void disassembleChunk(Chunk* chunk, const char* name) {
     printf("== %s ==\n", name);
@@ -20,13 +21,42 @@ static int simpleInstruction(const char* name, int offset) {
     return offset + 1;
 }
 
+static int constantInstruction(const char* name, Chunk* chunk, int offset) {
+    uint8_t constant = chunk->code[offset + 1];
+    printf("%-16s %4d '", name, constant);
+    printValue(chunk->constants.values[constant]);
+    printf("'\n");
+    return offset + 2;
+}
+
+static int longConstantInstruction(const char* name, Chunk* chunk, int offset) {
+    uint32_t constant = chunk->code[offset + 1] |
+                       (chunk->code[offset + 2] << 8) |
+                       (chunk->code[offset + 3] << 16);
+    
+    printf("%-16s %4d '", name, constant);
+    printValue(chunk->constants.values[constant]);
+    printf("'\n");
+    return offset + 4;
+}
+
 int disassembleInstruction(Chunk* chunk, int offset) {
     printf("%04d ", offset);
+    int line = getLine(chunk, offset);
+    if (offset > 0 && line == getLine(chunk, offset - 1)) {
+      printf("   | ");
+    } else {
+      printf("%4d ", line);
+    }
     
     uint8_t instruction = chunk->code[offset];
     switch (instruction) {
         case OP_RETURN:
             return simpleInstruction("OP_RETURN", offset);
+        case OP_CONSTANT:
+            return constantInstruction("OP_CONSTANT", chunk, offset);
+        case OP_CONSTANT_LONG:
+            return longConstantInstruction("OP_CONSTANT_LONG", chunk, offset);
         default:
             printf("Unknown opcode %d\n", instruction);
             return offset + 1;
