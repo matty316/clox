903ba5a3d4ea28e4a3b9ed04351485e8d0b819aa
matty <4nem@matty316.com>
2023-10-31 13:27:31 -0500

    add negate and arithmetic ops

--- clox/common.h
+++ clox/common.h
@@ -12,5 +12,6 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#define DEBUG_TRACE_EXECUTION
 
 #endif /* common_h */
---
+++ clox/vm.h
@@ -0,0 +1,35 @@
+//
+//  vm.h
+//  clox
+//
+//  Created by matty on 10/31/23.
+//
+
+#ifndef vm_h
+#define vm_h
+
+#include "chunk.h"
+#include "value.h"
+
+#define STACK_MAX 256
+
+typedef struct {
+    Chunk* chunk;
+    uint8_t* ip;
+    Value stack[STACK_MAX];
+    Value* stackTop;
+} VM;
+
+typedef enum {
+    INTERPRET_OK,
+    INTERPRET_COMPILE_ERROR,
+    INTERPRET_RUNTIME_ERROR,
+} InterpretResult;
+
+void initVM(void);
+void freeVM(void);
+InterpretResult interpret(Chunk *chunk);
+void push(Value value);
+Value pop(void);
+
+#endif /* vm_h */
